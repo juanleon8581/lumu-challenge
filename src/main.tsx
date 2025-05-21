ef772dd4fec8f00f64517ccdfeeb4c49d0a1a94b
Juan Leon <juanleon8581@gmail.com>
2025-05-20 22:24:07 -0500

    Wrap Home component with QueryClientProvider for tanstack query integration

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,14 @@ import { createRoot } from "react-dom/client";
 import { Home } from "./presentation/pages/Home";
 
 import "@/presentation/styles/general.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Home />
+    <QueryClientProvider client={queryClient}>
+      <Home />
+    </QueryClientProvider>
   </StrictMode>
 );
