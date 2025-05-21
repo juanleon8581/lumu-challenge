7a0cb03a06c204f2c080a28ef1dddd6b18a54566
Juan Leon <juanleon8581@gmail.com>
2025-05-21 00:48:03 -0500

    Reorder import statements for consistency

--- src/presentation/main.tsx
+++ src/presentation/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Home } from "@/presentation/pages/Home";
-
-import "@/presentation/styles/general.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./config/timeStackClient";
 import { DashboardMenu } from "./components/dashboard/DashboardMenu";
+
+import "@/presentation/styles/general.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
