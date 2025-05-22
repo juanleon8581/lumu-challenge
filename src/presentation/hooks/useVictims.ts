c813cd364ac3304815b5f15c8d06123f93ea5ed6
Juan Leon <juanleon8581@gmail.com>
2025-05-22 01:23:48 -0500

    refactor: remove unused Zustand store references in useVictims hook

--- src/presentation/hooks/useVictims.ts
+++ src/presentation/hooks/useVictims.ts
@@ -1,16 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
-import { useEffect } from "react";
 import { VictimsRepositoryImpl } from "@/infrastructure/api/victims.repository.impl";
 import { GetVictims } from "@/domain/use-cases/GetVictims";
-import { useVictimsStore } from "../store/victimsStore";
 
 const victimsRepository = new VictimsRepositoryImpl();
 const getVictimsUseCase = new GetVictims(victimsRepository);
 
 export const useVictims = () => {
-  // const victims = useVictimsStore((state) => state.victims);
-  // const setVictims = useVictimsStore((state) => state.setVictims);
-
   const { data, isLoading, error, refetch } = useQuery({
     queryKey: ["victims"],
     queryFn: () => getVictimsUseCase.execute(),
